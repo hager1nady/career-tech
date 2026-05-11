1294bcd6a242412abd7bec4f6641050204e69c6f
basmala <basmala112@gmail.com>
2026-05-11 20:38:27 +0300

    add yup validation

--- Careertech2/src/component/Login/Login.jsx
+++ Careertech2/src/component/Login/Login.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import { useFormik } from 'formik'
-
 import { useState } from 'react'
+import * as Yup from 'yup'
+const validationSchema = Yup.object({
+  email: Yup.string()
+    .email('Enter a valid email address')
+    .required('Email is required'),
+  password: Yup.string()
+    .min(6, 'Password must be at least 6 characters')
+    .required('Password is required'),
+})
 
 export default function Login() {
    
