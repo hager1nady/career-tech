fca8c07de98f1cb875cb93c227766c92c700ff7a
weam ahmed <weamahmed@gmail.com>
2026-05-11 19:16:07 +0300

    update project

--- src/component/test/test.jsx
+++ src/component/test/test.jsx
@@ -3,7 +3,12 @@ import Register from "./Register";
 function App() {
   return (
     <div>
-      <Register />
+      <P>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum doloribus
+        tempora delectus cupiditate, molestias dignissimos dolorem, sapiente hic
+        numquam quasi corporis voluptatum temporibus porro sint, eligendi ab
+        impedit ut culpa.
+      </P>
     </div>
   );
 }
