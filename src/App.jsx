cf959a3d77228f9c70b3c04bb95b6df500e80dd0
hager nady <hagernady22@gmail.com>
2026-05-12 20:12:45 +0300

    update register

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-<<<<<<< HEAD
-import AiChat from './Components/AIChat/AIChat'
-=======
+
 import Register from './pages/auth/Register/Register'
 import 'bootstrap/dist/css/bootstrap.min.css'
->>>>>>> c07ea602e0394f316f3aadf3a75d0341377d9cb4
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-<<<<<<< HEAD
-     <AiChat/>
-=======
       <Register />
->>>>>>> c07ea602e0394f316f3aadf3a75d0341377d9cb4
     </>
   )
 }
